75d3d08ed2ee9b0a60586df534354c1c97dad521
KripaVS <kripa.ev@inapp.com>
2022-08-05 11:57:53 +0530

    not done yet

--- assignments/class_js.js
+++ assignments/class_js.js
@@ -1,18 +1,23 @@
-class student
-{constructor(name,studclass,age,gender,loc)
-    {   
-        this.name=name
+class Student
+{
+    constructor(name,studclass,age,gender,loc)
+    {   this.name=name
         this.studclass=studclass
         this.age=age
         this.gender=gender
         this.loc=loc
     }
-    addstud()
-    {
-        this.name=prompt("Enter the student name:")
+    addstudent()
+    {   this.name=prompt("Enter the student name:")
         this.studclass=prompt("Enter the student class:")
         this.age=prompt("Enter the students age:")
         this.gender=prompt("Enter the gender:")
         this.loc=prompt("Enter the students location:")
     }
 }
+var fname=prompt("Enter the student name:")
+
+var varname=new Student
+varname.addstudent()
+
+
