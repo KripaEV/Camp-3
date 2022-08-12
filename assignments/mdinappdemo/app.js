9f7625274f0cb254774d5250820e9ca4b5d1f0b9
KripaVS <kripa.ev@inapp.com>
2022-08-12 09:17:28 +0530

    added materialize

---
+++ assignments/mdinappdemo/app.js
@@ -0,0 +1,4 @@
+//console.log("Hello World");
+//to make the button with a ripple 
+import {MDCRipple} from '@material/ripple/index';
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
